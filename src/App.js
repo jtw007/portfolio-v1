3fcdb22af0ebac66b0d7b435930667617a334391
jtw007 <joshwu095@gmail.com>
2023-02-20 16:52:51 -0800

    import font, home and navbar.jsx created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import {
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Link 
+} from 'react-router-dom'
+// components import
+import NavBar from './components/NavBar.jsx'
+
+// css file import
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='placeholder'>
+      <Router>
+        <header>
+          <NavBar />
+        </header>
+      </Router>
     </div>
   );
 }
