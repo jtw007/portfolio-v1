1bce103c6e0039a0685609ee30760d0240540c17
jtw007 <joshwu095@gmail.com>
2023-02-22 15:45:55 -0800

    reverted navbar back to bootstrap

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import {
 
 
 // css file import
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import './index.css'
 
@@ -15,9 +15,9 @@ import './index.css'
 import NavBar from './components/NavBar.jsx'
 function App() {
   return (
-    <div className='body d-flex flex-column min-vh-100 m-auto'>
+    <div className='body'>
       <Router>
-        <header className='header'>
+        <header>
           <NavBar />
         </header>
       </Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,27 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar() {
 
     return(
-        <nav className='navbar'>
-            <div className='nav-brand'>
-                <div id='brand'>Josh.Dev</div> 
-            </div>
+        <Navbar className="d-flex" bg="dark" expand="lg">
+        <Container className="d-flex">
+            <Navbar.Brand style={{ color: 'rgba(50, 205, 50)'}}>Josh Wu</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+
             
-            <ul className="nav-links ">
-                <li className='links' id='link-home'><a href="#">Home</a></li>
-                <li className='links' id='link-about'><a href="#">About</a></li>
-                <li className='links' id='link-projects'><a href="#">Projects</a></li>
-                <li className='links' id='link-contact'><a href="#">Contact</a></li>
-            </ul>
-        </nav>
-        
+            <Nav className="me-auto d-flex justify-content-center justify-content-around w-100">
+                <Nav.Link style={{ color: 'rgba(50, 205, 50)'}} href="#">Home</Nav.Link>
+                <Nav.Link style={{ color: 'rgba(50, 205, 50)'}} href="#">Projects</Nav.Link>
+                <Nav.Link style={{ color: 'rgba(50, 205, 50)'}} href="#">About</Nav.Link>
+                <Nav.Link style={{ color: 'rgba(50, 205, 50)'}} href="#">Contact</Nav.Link>
+            </Nav>
+            
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>  
     )
 }
