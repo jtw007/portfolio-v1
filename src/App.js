5738a6f8b0d98ca3d2f8843f16f3680c60e01772
jtw007 <joshwu095@gmail.com>
2023-02-22 13:40:47 -0800

    updated navbar and styles

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import {
   BrowserRouter as Router,
   Link 
 } from 'react-router-dom'
-// components import
-import NavBar from './components/NavBar.jsx'
+
 
 // css file import
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './index.css'
 
-
+// components import
+import NavBar from './components/NavBar.jsx'
 function App() {
   return (
-    <div className='placeholder'>
+    <div className='body d-flex flex-column min-vh-100 m-auto'>
       <Router>
-        <header>
+        <header className='header'>
           <NavBar />
         </header>
       </Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,27 +5,18 @@ import Navbar from 'react-bootstrap/Navbar';
 export default function NavBar() {
 
     return(
-        // <Navbar style={{ backgroundColor: 'rgba(0,68,129)' }} variant="dark" expand="lg">
-		// 	<Container>
-        //         <Navbar.Brand href="/">Josh.Dev</Navbar.Brand>
-		// 		<Navbar.Toggle aria-controls="basic-navbar-nav" />
-		// 		<Navbar.Collapse id="basic-navbar-nav">
-		// 		<Nav className="me-right">
-		// 			<Nav.Link href="#">link 1</Nav.Link>
-		// 			<Nav.Link href="#">link 2</Nav.Link>
-		// 		</Nav>
-		// 		</Navbar.Collapse>
-		// 	</Container>
-   		// </Navbar>
-        <Navbar style={{ backgroundColor: 'rgba(0,68,129)' }} variant="dark" expand="lg">
-        <Container>
-            <Navbar.Brand href="/">Josh.Dev</Navbar.Brand>
-            <Nav className="me-auto">
-            <Nav.Link href="#">Link 1</Nav.Link>
-            <Nav.Link href="#">Link 2</Nav.Link>
-            <Nav.Link href="#">Link 3</Nav.Link>
-            </Nav>
-        </Container>
-        </Navbar>
+        <nav className='navbar'>
+            <div className='nav-brand'>
+                <div id='brand'>Josh.Dev</div> 
+            </div>
+            
+            <ul className="nav-links ">
+                <li className='links' id='link-home'><a href="#">Home</a></li>
+                <li className='links' id='link-about'><a href="#">About</a></li>
+                <li className='links' id='link-projects'><a href="#">Projects</a></li>
+                <li className='links' id='link-contact'><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+        
     )
 }
