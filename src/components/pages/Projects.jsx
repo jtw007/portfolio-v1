f570f5724892eba31fdf4f94e3d8b066d45e6647
jtw007 <joshwu095@gmail.com>
2023-03-07 16:31:10 -0800

    cannot get project-overlay-container contents to render on hover

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -3,18 +3,18 @@ import { Link } from "react-router-dom";
 import { useState } from 'react';
 
 const Projects = () => {
-    // const [ style, setStyle ] = useState({display: 'none'})
+    const [ style, setStyle ] = useState({display: 'none'})
     
-    // const linkStyle = {
-    //     textDecoration: 'none',
-    // }
-    // const projectsArray = projectList?.map((project, idx) => {
-    //     return(
-    //         <div className='project-container' key={`project-${idx}`}>
-    //             <img className='project-img' src={project.img} />
-    //         </div>
-    //     )
-    // })
+    const linkStyle = {
+        textDecoration: 'none',
+    }
+    const projectsArray = projectList?.map((project, idx) => {
+        return(
+            <div className='project-container' key={`project-${idx}`}>
+                <img className='project-img' src={project.img} />
+            </div>
+        )
+    })
 
     return (
 
@@ -23,37 +23,37 @@ const Projects = () => {
             <div className='project-container'>
                 {projectList.map((project, idx) => {
                     return(
-                        // <Link key={`project-${idx}`} to={`/${project.slug}`} style={ linkStyle }>
+                        <Link key={`project-${idx}`} to={`/${project.slug}`} style={ linkStyle }>
                             <div id='container'>
                                 <div id='test-container'>
                                     <img 
-                                    // onMouseEnter={e => {
-                                    //     setStyle({display: 'block'})
-                                    // }}
-                                    // onMouseLeave={e => {
-                                    //     setStyle({display: 'none'})
-                                    // }} 
+                                    onMouseEnter={e => {
+                                        setStyle({display: 'block'})
+                                    }}
+                                    onMouseLeave={e => {
+                                        setStyle({display: 'none'})
+                                    }} 
                                     className='project-img' 
                                     src={project.img} 
                                     alt='' />
                                     <div className="project-overlay">
                                         <div className='project-overlay-container'>
-                                            {/* <span className="project-span" style={style}>
+                                            <span className="project-span" style={style}>
                                                 {project.name}
                                             </span>
-                                            <button className="projectbtn" style={style}>See more</button> */}
+                                            <button className="projectbtn" style={style}>See more</button>
                                             {/* TEMP */}
-                                            <span className="project-span" >
+                                            {/* <span className="project-span" >
                                                 {project.name}
                                             </span>
                                             <Link key={`project-${idx}`} to={`/${project.slug}`}>
                                             <button className="projectbtn">See more</button>
-                                            </Link>
+                                            </Link> */}
                                         </div>  
                                     </div>
                                 </div>
                             </div>
-                        // </Link>
+                        </Link>
                             
                     ) 
                 })}
