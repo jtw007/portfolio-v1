9b0e727c6ace27528cd276069a87afedb7e52ec2
jtw007 <joshwu095@gmail.com>
2023-04-03 10:32:14 -0700

    updated resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,7 +20,7 @@ export default function NavBar() {
                 
                 <Nav className="nav-links me-auto d-flex justify-content-center justify-content-around w-100">
                     <Nav.Link className='nav-link fs-5' style={{ color: 'rgba(207, 207, 207)' }} href="/">Home</Nav.Link>
-                    <Nav.Link className='nav-link fs-5' style={{ color: 'rgba(207, 207, 207)' }} href='https://drive.google.com/file/d/1xhq4NDOy1EYdpKJqeutz4eL5BW8JXIOu/view?usp=share_link' target="_blank" rel="noopener noreferrer">Resume</Nav.Link>
+                    <Nav.Link className='nav-link fs-5' style={{ color: 'rgba(207, 207, 207)' }} href='https://drive.google.com/file/d/1OmLrMYX7Arm-7v2XwvtuFdU8BgKadRmM/view?usp=sharing' target="_blank" rel="noopener noreferrer">Resume</Nav.Link>
                     {/* <Nav.Link className='nav-link' style={{ color: 'rgba(207, 207, 207)' }} href="#">About</Nav.Link>
                     <Nav.Link className='nav-link' style={{ color: 'rgba(207, 207, 207)' }} href="#">Projects</Nav.Link> */}
                     <NavDropdown 
